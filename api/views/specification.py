9e280b32f2643cccb1050ef5d031ac62e53dc17c
tomioka-k <tomioka-k@tajima.co.jp>
2021-11-15 08:23:37 +0900

    debug

--- api/views/specification.py
+++ api/views/specification.py
@@ -3,22 +3,22 @@ from rest_framework import generics
 from rest_framework import pagination
 from django_filters import rest_framework as filters
 from database.models.material import Material
-from database.models.specification import Specification, SpecificationCompose
+from database.models.specification import Specification, SpecificationCompose, Walk
 from database.models.document import SpecificationDocument
 from api.serializers.database import SpecificationDocumentSerializer, SpecificationProcessSerializer, SpecificationSerializer, SpecificationDetailSerializer
 from api.filters import SpecificationFilter
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 20
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 100
 
 
 class SpecificationListAPIView(generics.ListAPIView):
     queryset = Specification.objects.select_related(
         'walk', 'method', 'part', 'method__category', 'paste'
-    ).prefetch_related('base').filter(is_display=True)
+    ).prefetch_related('base', 'paste').filter(is_display=True)
     serializer_class = SpecificationSerializer
     pagination_class = CustomPagination
     filter_backends = (filters.DjangoFilterBackend,)
